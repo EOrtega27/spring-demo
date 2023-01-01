d83d41ccc8527733e1431a5f31c6e58a5163f071
EOrtega27 <ortegachaveznsm@gmail.com>
2022-12-31 20:26:31 -0500

    first commit

---
+++ src/test/java/ed/com/oc/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ed.com.oc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
